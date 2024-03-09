af4c3d5f4f66dd61235933844697d6a44163b945
DELL LATITUDE <DELL LATITUDE@DESKTOP-AH5NTVP>
2024-03-09 22:53:39 +0500

    Add project files.

---
+++ Controllers/ADMIN/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NEXUS.Controllers.ADMIN
+{
+	public class AdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Controllers/WEB/WEBController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NEXUS.Controllers.WEB
+{
+    public class WEBController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Price()
+        {
+            return View();
+        }
+    }
+}
